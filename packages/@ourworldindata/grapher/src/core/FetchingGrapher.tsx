7507c25aa0efff6b1b98bd6392e5a479b3937e1e
Daniel Bachler <daniel@danielbachler.de>
2025-01-14 10:54:45 +0100

    :construction: wip try to figure out why entity selector panel is missing

--- packages/@ourworldindata/grapher/src/core/FetchingGrapher.tsx
+++ packages/@ourworldindata/grapher/src/core/FetchingGrapher.tsx
@@ -38,6 +38,7 @@ export function FetchingGrapher(
 
     // update grapherState when the config from props changes
     React.useEffect(() => {
+        console.log("updating grapherState bounds", props.config?.bounds)
         if (props.config?.bounds)
             grapherState.current.externalBounds = props.config.bounds
     }, [props.config?.bounds])
